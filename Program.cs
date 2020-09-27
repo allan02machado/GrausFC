b794cf8ef2fa6917fd1f7d1d014ed309f1296341
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-09-27 20:05:59 -0300

    Este commit altera a página inicial do repositório

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorInserido;
+            const double n1 = 32;
+            const double n2 = 5;
+            const double n3 = 9;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Converta °F em °C");
+            Console.ResetColor();
+            Console.WriteLine("Pressione ENTER para continuar...");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Digite o valor desejado: ");
+
+            Console.Write("Grau Fahrenheit: ");
+            valorInserido = Convert.ToDouble(Console.ReadLine());
+            double resultado = (valorInserido - n1) * n2 / n3;
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{valorInserido} °F equivalem a {resultado:N2} °C");
+            Console.ResetColor();
+            }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # GrausFC
 Resposta ao exercício "GrausFC"
+
+
+**Enunciado**
+
+Converta uma temperatura digitada pelo usuário em °F para °C.
+
+**Como executar**
+
+*Abra o programa e digite:*
+
+dotnet run
+
